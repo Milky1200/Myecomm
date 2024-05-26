c8010d87703ce1b83903d8957fbd49eea861465c
Milky1200 <71332948+Milky1200@users.noreply.github.com>
2024-05-26 15:33:27 +0530

    Session Maintained By SharedPreferences

--- app/src/main/java/com/mishraaditya/mycomm/Activities/LoginActivity.java
+++ app/src/main/java/com/mishraaditya/mycomm/Activities/LoginActivity.java
@@ -18,6 +18,7 @@ import androidx.core.view.WindowInsetsCompat;
 import com.mishraaditya.mycomm.ModelResponse.LoginResponse;
 import com.mishraaditya.mycomm.R;
 import com.mishraaditya.mycomm.RetrofitClient;
+import com.mishraaditya.mycomm.SharedPrefManager;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -29,6 +30,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     AppCompatButton btnLogin;
     TextView registerLink;
     EditText email,password;
+    SharedPrefManager sharedPrefManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -48,6 +50,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
         registerLink.setOnClickListener(this);
         btnLogin.setOnClickListener(this);
+        sharedPrefManager=new SharedPrefManager(getApplicationContext());
 
 
 
@@ -96,10 +99,15 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
             public void onResponse(Call<LoginResponse> call, Response<LoginResponse> response) {
                 LoginResponse loginResponse=response.body();
                 if(response.isSuccessful()){
-                    Toast.makeText(LoginActivity.this,loginResponse.getMessage(),Toast.LENGTH_SHORT).show();
-                    Intent iNext=new Intent(LoginActivity.this, HomeActivity.class);
-                    iNext.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                    startActivity(iNext);
+                    sharedPrefManager.saveUser(loginResponse.getUser());
+                    if(loginResponse.getError().equals("000")){
+                        Toast.makeText(LoginActivity.this,loginResponse.getMessage(),Toast.LENGTH_SHORT).show();
+                        Intent iNext=new Intent(LoginActivity.this, HomeActivity.class);
+                        iNext.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                        startActivity(iNext);
+                    }else {
+                        Toast.makeText(LoginActivity.this,loginResponse.getMessage(),Toast.LENGTH_SHORT).show();
+                    }
                 }
                 else {
                     Toast.makeText(LoginActivity.this,"Failure Occurred",Toast.LENGTH_LONG).show();
@@ -118,5 +126,14 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     private void switchOnRegister() {
         Intent i=new Intent(LoginActivity.this, MainActivity.class);
         startActivity(i);
+    }
+
+    protected void onStart(){
+        super.onStart();
+        if(sharedPrefManager.isLogged()){
+            Intent iNext=new Intent(LoginActivity.this, HomeActivity.class);
+            iNext.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(iNext);
+        }
     }
 }
--- app/src/main/java/com/mishraaditya/mycomm/RetrofitClient.java
+++ app/src/main/java/com/mishraaditya/mycomm/RetrofitClient.java
@@ -1,5 +1,10 @@
 package com.mishraaditya.mycomm;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import okhttp3.OkHttpClient;
+
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -7,7 +12,6 @@ public class RetrofitClient {
     public static String BASE_URL="http://192.168.1.37/userApi/";
     private static RetrofitClient retrofitClient;
     private static Retrofit retrofit;
-
 
     private RetrofitClient(){
         retrofit=new Retrofit.Builder()
